6232df3943752f80aefd0a67f3a084ef56959ba8
NivaldoFarias <badivia.f@gmail.com>
2024-08-16 11:40:09 -0300

    chore: encapsule app setup within function

--- src/email.ts
+++ src/email.ts
@@ -8,48 +8,55 @@ import { Resource } from "sst";
 import { env } from "./env";
 import { awsErrorSchema, emailInputSchema } from "./schemas";
 
-const client = new SESv2Client();
+export const handler = handle(setup());
 
-const app = new Hono();
+function setup() {
+	const client = new SESv2Client();
 
-app.use(prettyJSON());
-app.post(
-	"/email",
-	zValidator("json", emailInputSchema, (result, ctx) => {
-		if (!result.success) return ctx.json(result.error, 400);
-	}),
-	async (ctx) => {
-		try {
-			const input = ctx.req.valid("json");
+	const app = new Hono();
 
-			await client.send(
-				new SendEmailCommand({
-					FromEmailAddress: Resource.modakEmail.sender,
-					Destination: { ToAddresses: [input.destination] },
-					Content: {
-						Simple: {
-							Subject: { Data: `[${input.type}] New SST App Notification` },
-							Body: {
-								Text: {
-									Data: `Sent from my SST app '${Resource.App.name}' on stage '${Resource.App.stage}' and environment '${env.NODE_ENV}'. \n\n${input.message}`,
+	app.use(prettyJSON());
+	app.post(
+		"/email",
+		zValidator("json", emailInputSchema, (result, ctx) => {
+			if (!result.success) return ctx.json(result.error, 400);
+		}),
+		async (ctx) => {
+			try {
+				const input = ctx.req.valid("json");
+
+				await client.send(
+					new SendEmailCommand({
+						FromEmailAddress: Resource.modakEmail.sender,
+						Destination: { ToAddresses: [input.destination] },
+						Content: {
+							Simple: {
+								Subject: { Data: `[${input.type}] New Notification` },
+								Body: {
+									Text: {
+										Data: `Sent from my '${Resource.App.name}' on stage '${Resource.App.stage}' and environment '${env.NODE_ENV}'. \n\n${input.message}`,
+									},
 								},
 							},
 						},
-					},
-				}),
-			);
+					}),
+				);
 
-			return ctx.json({ message: "ok" });
-		} catch (error) {
-			const awsError = awsErrorSchema.safeParse(error);
+				return ctx.json({ message: "ok" });
+			} catch (error) {
+				const awsError = awsErrorSchema.safeParse(error);
 
-			if (awsError.success) {
-				return ctx.json({ message: awsError.data.message }, awsError.data.$metadata.httpStatusCode);
+				if (awsError.success) {
+					return ctx.json(
+						{ message: awsError.data.message },
+						awsError.data.$metadata.httpStatusCode,
+					);
+				}
+
+				return ctx.json({ message: error instanceof Error ? error.message : error }, 500);
 			}
+		},
+	);
 
-			return ctx.json({ message: error instanceof Error ? error.message : error }, 500);
-		}
-	},
-);
-
-export const handler = handle(app);
+	return app;
+}
